63ee548c69e7cab9f7048137f7d2d2e101860f5b
kpejiga <kpejiga@yahoo.co.jp>
2017-10-05 07:55:03 +0900

    Add ex_three.rb

---
+++ the_basics/ex_three.rb
@@ -0,0 +1,13 @@
+# Using a hash to store a list of movie titles...
+
+moviez = {:movie_a => 1234, 
+          :movie_b => 1233, 
+          :movie_c => 1220, 
+          :movie_d => 2118, 
+          :movie_e => 2017}
+          
+puts moviez[:movie_a]
+puts moviez[:movie_b]
+puts moviez[:movie_c]
+puts moviez[:movie_d]
+puts moviez[:movie_e]
