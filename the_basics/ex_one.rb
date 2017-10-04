a82b77cb04e066b3733bf481f278d2e4ad46ddcf
kpejiga <kpejiga@yahoo.co.jp>
2017-10-05 06:18:57 +0900

    Add ex_one.rb

---
+++ the_basics/ex_one.rb
@@ -0,0 +1,11 @@
+# Adding two strings together...
+
+# first name
+first_name = 'Nuhu'
+
+# last name
+last_name = 'Hamidu'
+
+# concatenating strings in one string - full name
+full_name = "#{first_name} #{last_name}"
+puts full_name
