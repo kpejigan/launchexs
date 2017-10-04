71a849c5cc7fbd07c38834f916872f908b1b81e0
kpejiga <kpejiga@yahoo.co.jp>
2017-10-05 07:39:16 +0900

    Add ex_two.rb

---
+++ the_basics/ex_two.rb
@@ -0,0 +1,11 @@
+# thousands place
+puts thousands = 4567 / 1000
+
+# hundreds place 
+puts hundreds = 4567 % 1000 / 100
+
+# tens place
+puts tens = 4567 % 1000 % 100 / 10
+
+# ones place 
+puts ones = 4567 % 10 
