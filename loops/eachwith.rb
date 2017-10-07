faed36989a152ad19e5dae0e5689ce3de52e6cdf
kpejiga <kpejiga@yahoo.co.jp>
2017-10-08 00:43:55 +0900

    Get done with loops

---
+++ loops/eachwith.rb
@@ -0,0 +1,5 @@
+array = [2, 3, 5, 8, 9]
+
+array.each_with_index do |x, i|
+  puts "#{i}, #{x}"
+end
---
+++ loops/stop.rb
@@ -0,0 +1,6 @@
+user_input = gets.chomp.upcase
+
+while user_input != 'STOP'
+  puts "I am not sure what you want me to do"
+  user_input = gets.chomp.upcase
+end
